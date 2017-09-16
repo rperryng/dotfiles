7410da54e67997654be7eab0f4cb3d81e49ff6f8
Ryan Perry-Nguyen <ryanperrynguyen@gmail.com>
2017-09-16 02:37:55 -0400

    separate os specific configs into their own files

---
+++ shell_extras.sh
@@ -0,0 +1,7 @@
+# global gitignore
+git config --global core.excludesfile ~/.gitignore
+
+# https://github.com/scmbreeze/scm_breeze
+# https://github.com/rupa/z
+# https://github.com/BurntSushi/ripgrep
+# http://www.rushiagr.com/blog/2016/06/16/everything-you-need-to-know-about-tmux-copy-pasting-ubuntu/
