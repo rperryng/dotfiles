4ba1d9c2b46bc784982d8dd38f85e1336ea7edeb
Ryan Perry-Nguyne <ryanperrynguyen@gmail.com>
2017-09-24 19:26:27 -0400

    Clean up shell_extras

--- shell_extras.sh
+++ shell_extras.sh
@@ -1,7 +1,10 @@
 # global gitignore
 git config --global core.excludesfile ~/.gitignore
 
-# https://github.com/scmbreeze/scm_breeze
-# https://github.com/rupa/z
-# https://github.com/BurntSushi/ripgrep
 # http://www.rushiagr.com/blog/2016/06/16/everything-you-need-to-know-about-tmux-copy-pasting-ubuntu/
+# https://github.com/BurntSushi/ripgrep
+# https://github.com/denysdovhan/spaceship-zsh-theme
+# https://github.com/rupa/z
+# https://github.com/scmbreeze/scm_breeze
+# https://github.com/tmux-plugins/tpm
+# https://github.com/zsh-users/zsh-syntax-highlighting
