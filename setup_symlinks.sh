5c9ea93c52435cdf7c714bb5c80bd7321f4a8670
Ryan Perry-Nguyen <ryanperrynguyen@gmail.com>
2018-05-16 00:01:46 -0400

    Include basic symlinking script

---
+++ setup_symlinks.sh
@@ -0,0 +1,16 @@
+DOTFILES_SOURCE=$(pwd)
+echo $DOTFILES_SOURCE
+
+ln -s $DOTFILES_SOURCE/zshrc.extras ~/.zshrc.extras
+echo "source ~/.zshrc.extras" >> ~/.zshrc
+
+ln -s $DOTFILES_SOURCE/tmux.conf ~/.tmux.conf
+ln -s $DOTFILES_SOURCE/tmux.conf.ui ~/.tmux.conf.ui
+ln -s $DOTFILES_SOURCE/tmux.conf.ubuntu ~/.tmux.conf.ubuntu
+ln -s $DOTFILES_SOURCE/tmux.conf.osx ~/.tmux.conf.osx
+
+ln -s $DOTFILES_SOURCE/vimrc ~/.vimrc
+mkdir -p ~/.config/nvim
+ln -s $DOTFILES_SOURCE/vimrc ~/.config/nvim/init.vim
+
+
