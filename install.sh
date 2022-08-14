484f801a3c18dc0c0af8c11d6989f2ee2478fac8
Ryan Perry-Nguyen <ryanperrynguyen@gmail.com>
2022-08-14 00:42:13 -0400

    nvim config; add prepend_path lib

--- install.sh
+++ install.sh
@@ -139,6 +139,37 @@ install_prompt() {
   esac
 }
 
+install_neovim() {
+  if command -v nvim; then
+    echo "neovim already installed"
+    return
+  fi
+
+  case ${os_family} in
+    "macos")
+      install_packages \
+        'ninja libtool automake cmake pkg-config gettext curl'
+      ;;
+    "debian")
+      install_packages \
+        'ninja-build gettext libtool libtool-bin autoconf automake cmake g++ pkg-config unzip curl doxygen'
+      ;;
+    *)
+      echo "OS family: '${os_family}' not supported"
+      exit 1
+      ;;
+  esac
+
+  if [[ ! -d "$HOME/.neovim" ]]; then
+    git clone https://github.com/neovim/neovim.git $(HOME)/.neovim
+  fi
+
+  pushd $(HOME)/.neovim >/dev/null
+  make CMAKE_BUILD_TYPE=Release
+  $sudo_cmd make install
+  popd >/dev/null
+}
+
 install_zsh_plugins() {
   mkdir -p ~/.zsh
 
@@ -149,7 +180,7 @@ install_zsh_plugins() {
       ~/.zsh-syntax-highlighting
   fi
 
-  if [[ ! -d ~/zsh-autosuggestions ]]; then
+  if [[ ! -d ~/.zsh-autosuggestions ]]; then
     git clone \
       'https://github.com/zsh-users/zsh-autosuggestions.git' \
       ~/.zsh-autosuggestions
@@ -214,6 +245,8 @@ main() {
   # TODO: Neovim nnn
 
   install_packages "fasd fzf"
+
+  install_neovim
 
   install_zsh_plugins
   install_prompt
