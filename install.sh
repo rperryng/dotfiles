2a76e0166e1fd62d63d55014d76904cf96a263ae
Ryan Perry-Nguyen <ryanperrynguyen@gmail.com>
2022-08-14 17:11:27 -0400

    use standard install script name

--- install.sh
+++ install.sh
@@ -141,38 +141,8 @@ install_prompt() {
 
 install_default_packages() {
   modules/nvim/install
-  modules/zsh-autosuggestions/install.zsh
-}
-
-install_neovim() {
-  if command -v nvim; then
-    echo "neovim already installed"
-    return
-  fi
-
-  case ${os_family} in
-    "macos")
-      install_packages \
-        'ninja libtool automake cmake pkg-config gettext curl'
-      ;;
-    "debian")
-      install_packages \
-        'ninja-build gettext libtool libtool-bin autoconf automake cmake g++ pkg-config unzip curl doxygen'
-      ;;
-    *)
-      echo "OS family: '${os_family}' not supported"
-      exit 1
-      ;;
-  esac
-
-  if [[ ! -d "$HOME/.neovim" ]]; then
-    git clone https://github.com/neovim/neovim.git ${HOME}/.neovim
-  fi
-
-  pushd $(HOME)/.neovim >/dev/null
-  make CMAKE_BUILD_TYPE=Release
-  $sudo_cmd make install
-  popd >/dev/null
+  modules/zsh-autosuggestions/install
+  modules/rg/install
 }
 
 clone_dotfiles() {
@@ -231,7 +201,6 @@ main() {
   install_packages "zsh"
 
   # TODO: Neovim nnn
-  # install_neovim
   install_prompt
   install_default_packages
 
