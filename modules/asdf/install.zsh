31732f95ece7c4dd39cab1ebf0edf8dd40eb5bba
Ryan Perry-Nguyen <ryanperrynguyen@gmail.com>
2022-08-21 02:18:53 -0400

    asdf ruby

--- modules/asdf/install.zsh
+++ modules/asdf/install.zsh
@@ -1,12 +1,11 @@
 #!/usr/bin/env zsh
 
 ASDF_REPOSITORY_URL="https://github.com/asdf-vm/asdf.git"
-ASDF_PLUGIN_NODEJS_URL="https://github.com/asdf-vm/asdf-nodejs.git"
 
 export ASDF_DIR="${XDG_OPT_HOME:-$HOME/.local/opt}/asdf"
-export ASDF_DATA_DIR="${XDG_DATA_HOME}/asdf"
-export ASDF_CONFIG_FILE="${XDG_CONFIG_HOME}/asdf/asdfrc"
-export ASDF_DEFAULT_TOOL_VERSIONS_FILENAME="${XDG_CONFIG_HOME}/asdf/tool-versions"
+export ASDF_DATA_DIR="${XDG_DATA_HOME:-$HOME/.local/share}/asdf"
+export ASDF_CONFIG_FILE="${XDG_CONFIG_HOME:-$HOME/.config}/asdf/asdfrc"
+export ASDF_DEFAULT_TOOL_VERSIONS_FILENAME="${XDG_CONFIG_HOME:-$HOME/.config}/asdf/tool-versions"
 
 install() {
   latest_tag=$( \
@@ -21,6 +20,7 @@ install() {
     --branch "${latest_tag}"
 }
 
+ASDF_PLUGIN_NODEJS_URL="https://github.com/asdf-vm/asdf-nodejs.git"
 install_nodejs() {
   case ${DOTFILES_OS} in
     "macos") install_packages 'gpg gawk' ;;
@@ -33,5 +33,28 @@ install_nodejs() {
   asdf install nodejs 16.17.0
 }
 
+ASDF_PLUGIN_RUBY_URL="https://github.com/asdf-vm/asdf-ruby.git"
+install_ruby() {
+  case ${DOTFILES_OS} in
+    "macos")
+      install_packages openssl@1.1 readline libyaml
+      export RUBY_CONFIGURE_OPTS="--with-openssl-dir=$(brew --prefix openssl@1.1)"
+      ;;
+    "debian")
+      install_packages \
+        'autoconf bison build-essential libssl-dev libyaml-dev ' \
+        'libreadline6-dev zlib1g-dev libncurses5-dev libffi-dev libgdbm6 ' \
+        'libgdbm-dev libdb-dev uuid-dev'
+      ;;
+    *) ;;
+  esac
+
+  asdf plugin add ruby ${ASDF_PLUGIN_RUBY_URL}
+  asdf install ruby
+
+  # TODO: add default-gems
+}
+
 install
 install_nodejs
+install_ruby
