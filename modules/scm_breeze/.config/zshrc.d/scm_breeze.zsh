6ef8d4eda51d5d1a96f848a91ef4bf6d5549a45a
Ryan Perry-Nguyen <ryanperrynguyen@gmail.com>
2022-08-21 21:49:38 -0400

    add scm breeze

---
+++ modules/scm_breeze/.config/zshrc.d/scm_breeze.zsh
@@ -0,0 +1,5 @@
+#!/usr/bin/env zsh
+
+# load scm_breeze
+SCM_BREEZE_PATH="${XDG_OPT_HOME}/scm_breeze/scm_breeze.sh"
+[ -s "${SCM_BREEZE_PATH}" ] && source "${SCM_BREEZE_PATH}"
---
+++ modules/scm_breeze/install.zsh
@@ -0,0 +1,14 @@
+#!/usr/bin/env zsh
+
+SCM_BREEZE_HOME="${XDG_OPT_HOME:-$HOME/.local/opt}/scm_breeze"
+CLONE_URL="https://github.com/scmbreeze/scm_breeze.git"
+
+install() {
+  git clone "${CLONE_URL}" "${SCM_BREEZE_HOME}"
+
+  pushd "${SCM_BREEZE_HOME}"
+  ./install.sh
+  popd
+}
+
+install
