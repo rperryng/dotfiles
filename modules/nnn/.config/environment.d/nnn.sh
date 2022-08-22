251337a90e07e4de9a822e807f8103258e792b7d
Ryan Perry-Nguyen <ryanperrynguyen@gmail.com>
2022-08-22 01:52:21 -0400

    add wsl bookmark for nnn

--- modules/nnn/.config/environment.d/nnn.sh
+++ modules/nnn/.config/environment.d/nnn.sh
@@ -1,4 +1,10 @@
+#!/usr/bin/env bash
+
 export NNN_OPTS="AH"
 export NNN_BMS='d:~/Downloads;w:~/code/ws;c:~/code;h:~'
 export NNN_NO_AUTOSELECT=1
 export NNN_COLORS=4321
+
+if is_wsl; then
+  export NNN_BMS="a:$(wslpath 'C:\Users\ryanp\home');${NNN_BMS}"
+fi
