cf3b40af7483b03b3ad6acb1346b8cfaee7805bd
Ryan Perry-Nguyen <ryanperrynguyen@gmail.com>
2022-08-22 01:51:56 -0400

    add more docker functions

--- modules/docker/.config/shell.d/docker.sh
+++ modules/docker/.config/shell.d/docker.sh
@@ -2,6 +2,20 @@
 
 alias dk="docker"
 
+function select_docker_container_id() {
+  local docker_container_ls_output=$(
+    docker container ls \
+      --format 'table {{.Names}}\t{{.ID}}\t{{.Status}}\t{{.RunningFor}}\t{{.Image}}' \
+  )
+
+  echo $docker_container_ls_output \
+    | tail -n+2 \
+    | fzf --header="$(echo $docker_container_ls_output | head -1)" \
+    | grep -oE '\s([a-zA-Z0-9]+)' \
+    | head -1 \
+    | grep -oE '([a-zA-Z0-9]+)'
+}
+
 function dlogs() {
   local container_id=$(select_docker_container_id)
   if [[ -n "${container_id}" ]]; then
@@ -11,3 +25,14 @@ function dlogs() {
   fi
 
   false
+}
+
+# Docker container id fuzzy
+function docker-container-widget() {
+  LBUFFER="${LBUFFER}$(select_docker_container_id)"
+  local rcode=$?
+  zle reset-prompt
+  return $rcode
+}
+zle -N docker-container-widget
+bindkey '^X^D' docker-container-widget
