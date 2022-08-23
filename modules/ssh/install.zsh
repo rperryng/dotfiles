6dc2371cb4275e593af0fa43d3b63b46ad08eac2
Ryan Perry-Nguyen <ryanperrynguyen@gmail.com>
2022-08-23 00:33:07 -0400

    fix wsl ssh key access

--- modules/ssh/install.zsh
+++ modules/ssh/install.zsh
@@ -3,23 +3,9 @@
 SSH_HOME="${HOME}/.ssh"
 
 install() {
-  if command -v nvim; then
-    echo "'fzf' command already available; skipping"
-    return
+  if [[ is_wsl ]]; then
+    sudo apt install keychain
   fi
-
-  # Install fzf
-  git clone \
-    --depth 1 \
-    https://github.com/junegunn/fzf.git \
-    "$FZF_HOME"
-
-  pushd $FZF_HOME
-  ./install \
-    --key-bindings \
-    --completion \
-    --no-updaterc
-  popd
 }
 
 install
