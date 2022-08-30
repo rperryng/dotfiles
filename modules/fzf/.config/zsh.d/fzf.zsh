f0479f1bfcc1ee0d9ccb8c41ebc94f304f9e1e34
Ryan Perry-Nguyen <ryanperrynguyen@gmail.com>
2022-08-30 12:41:26 -0400

    add fzf cd widget keybind

--- modules/fzf/.config/zsh.d/fzf.zsh
+++ modules/fzf/.config/zsh.d/fzf.zsh
@@ -2,3 +2,8 @@
 
 source "${XDG_OPT_HOME}/fzf/shell/completion.zsh"
 source "${XDG_OPT_HOME}/fzf/shell/key-bindings.zsh"
+
+# CTRL-Y - cd into the selected directory
+bindkey -M emacs '^Y' fzf-cd-widget
+bindkey -M vicmd '^Y' fzf-cd-widget
+bindkey -M viins '^Y' fzf-cd-widget
