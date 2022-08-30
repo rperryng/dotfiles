f5696b5ae1ab68457d3be83049e8cc11d7aa5f33
Ryan Perry-Nguyen <ryanperrynguyen@gmail.com>
2022-08-30 12:54:56 -0400

    fix command used for fzf-cd-widget

--- modules/fzf/.config/shell.d/fzf.sh
+++ modules/fzf/.config/shell.d/fzf.sh
@@ -2,6 +2,7 @@
 # FZF Configuration
 #
 export FZF_DEFAULT_COMMAND='rg --files --hidden --follow --glob "!.git"'
-export FZF_DEFAULT_OPTS="--height 40% --layout=reverse"
+export FZF_DEFAULT_OPTS="--height 70% --layout=reverse"
+export FZF_ALT_C_COMMAND='rg --files --hidden --follow --glob "!.git" | xargs dirname | sort -u'
 
 prepend_path "${XDG_OPT_HOME}/fzf/bin"
