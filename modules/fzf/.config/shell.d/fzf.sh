ba9329fc907791808bc7a2b7c1aee185c14edaf7
Ryan Perry-Nguyen <ryanperrynguyen@gmail.com>
2022-09-01 13:32:13 -0400

    chore: only set fzf opts if rg available

--- modules/fzf/.config/shell.d/fzf.sh
+++ modules/fzf/.config/shell.d/fzf.sh
@@ -1,8 +1,12 @@
 #
 # FZF Configuration
 #
-export FZF_DEFAULT_COMMAND='rg --files --hidden --follow --glob "!.git"'
 export FZF_DEFAULT_OPTS="--height 70% --layout=reverse"
-export FZF_ALT_C_COMMAND='rg --files --hidden --follow --glob "!.git" | xargs dirname | sort -u'
+
+if [ -x "$(command -v rg)" ]; then
+  export FZF_DEFAULT_COMMAND='rg --files --hidden --follow --glob "!.git"'
+  export FZF_ALT_C_COMMAND='rg --files --hidden --follow --glob "!.git" | xargs dirname | sort -u'
+  export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
+fi
 
 prepend_path "${XDG_OPT_HOME}/fzf/bin"
