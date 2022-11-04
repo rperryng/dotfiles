45a1d3166dc5889fb5c2c5d2918c7d6afdf77465
Ryan Perry-Nguyen <ryanperrynguyen@gmail.com>
2022-11-04 00:00:57 -0400

    add linuxbrew shell settings

--- modules/homebrew/.config/shell.d/brew.sh
+++ modules/homebrew/.config/shell.d/brew.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
 
-PATH="/home/linuxbrew/.linuxbrew/bin:${PATH}"
-eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+if [[ ${DOTFILES_OS} == "debian" ]]; then
+  PATH="/home/linuxbrew/.linuxbrew/bin:${PATH}"
+  eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+fi
