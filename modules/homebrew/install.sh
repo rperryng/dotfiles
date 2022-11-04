4aee1175dc748d06f651754c5609141b20a339d0
Ryan Perry-Nguyen <ryanperrynguyen@gmail.com>
2022-11-04 00:02:58 -0400

    cleanup

--- modules/homebrew/install.sh
+++ modules/homebrew/install.sh
@@ -13,26 +13,10 @@ if [[ ! -x "$(command -v brew)" ]]; then
     # Install standard homebrew
     /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
   elif [[ "${DOTFILES_OS}" == linux* ]]; then
-    # Make sure this is in the path
-    if [[ ! "$PATH" == */home/linuxbrew/.linuxbrew/bin* ]]; then
-      PATH="/home/linuxbrew/.linuxbrew/bin:${PATH}"
-    fi
-
-    # Install standard homebrew
     /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-    eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+
+    source "${DOTFILES_DIR}/modules/homebrew/.config/shell.d/brew.sh"
   fi
 else
   echo "brew already installed."
 fi
-
-# Install brews using brew bundle (uses the Brewfile)
-# if [[ -x "$(command -v brew)" ]]; then
-#   printf "Do you want run brew bundle [y/N]? "
-#   read -r answer
-#   case "${answer}" in [yY] | [yY][eE][sS])
-#     brewfile="${DOTFILES_DIR:-$HOME/.dotfiles}/modules/homebrew/.config/homebrew/Brewfile"
-#     HOMEBREW_BUNDLE_FILE="$brewfile" brew bundle
-#     ;;
-#   esac
-# fi
