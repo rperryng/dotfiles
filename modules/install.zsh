d4162a0b2f26c5881196115eb4d215d220f717ee
Ryan Perry-Nguyen <ryanperrynguyen@gmail.com>
2022-11-07 15:13:09 -0500

    better comment

--- modules/install.zsh
+++ modules/install.zsh
@@ -1,6 +1,7 @@
 #!/usr/bin/env zsh
 
 # stub file to invoke the real install.sh bash script
-# the root install.sh starts "zsh" in order to load all the expected environment
-# variables when installing the modules.
+# the root install.sh could be executed from bash, so using a `.zsh` here
+# at least ensures that the zsh environment variables/config etc are present
+# before installing all the modules.
 "${DOTFILES_DIR:-$HOME/.dotfiles}/modules/install.sh"
