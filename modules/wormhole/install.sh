d8ea883e549de73c709d8e7d4b3a6b75e86f0483
Ryan Perry-Nguyen <ryanperrynguyen@gmail.com>
2022-11-18 13:22:31 -0500

    wormhole

---
+++ modules/wormhole/install.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+set -e
+
+install() {
+  if [[ -x "$(command -v wormhole)" ]]; then
+    return 0;
+  fi
+
+  case ${DOTFILES_OS} in
+    "macos")
+      brew install magic-wormhole
+      ;;
+    "debian")
+      sudo apt install magic-wormhole
+      ;;
+    *)
+      echo "OS family: '${DOTFILES_OS}' not supported"
+      exit 1
+      ;;
+  esac
+}
+
+install
