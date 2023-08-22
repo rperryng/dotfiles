23b55e7137b47a95933a9ca6d3b77f201ab152b3
Ryan Perry-Nguyen <ryanperrynguyen@gmail.com>
2023-08-22 14:16:03 -0400

    chore: add small k8s alias

--- modules/k8s/.config/shell.d/k8s.sh
+++ modules/k8s/.config/shell.d/k8s.sh
@@ -4,12 +4,9 @@
 
 alias k="kubectl"
 alias kx='f() { [ "$1" ] && kubectl config use-context $1 || kubectl config current-context ; } ; f'
+alias kxa='kubectl config get-contexts'
 alias kn='f() { [ "$1" ] && kubectl config set-context --current --namespace $1 || kubectl config view --minify | grep namespace | cut -d" " -f6 ; } ; f'
 alias kna='kubectl get namespace --all-namespaces'
-
-kn_() {
-
-}
 
 function get_column_data() {
     # Read the first line of STDIN as the header
