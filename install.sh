7858f4dfa993ea27ddf52534a4a057244afeba91
Ryan Perry-Nguyen <ryanperrynguyen@gmail.com>
2023-10-31 23:23:16 -0400

    chore: add prebuilt mpr

--- install.sh
+++ install.sh
@@ -52,6 +52,11 @@ function is_wsl() {
   return 1
 }
 
+setup_debian_prebuilt_mpr() {
+  wget -qO - 'https://proget.makedeb.org/debian-feeds/prebuilt-mpr.pub' | gpg --dearmor | sudo tee /usr/share/keyrings/prebuilt-mpr-archive-keyring.gpg 1> /dev/null
+  echo "deb [arch=all,$(dpkg --print-architecture) signed-by=/usr/share/keyrings/prebuilt-mpr-archive-keyring.gpg] https://proget.makedeb.org prebuilt-mpr $(lsb_release -cs)" | sudo tee /etc/apt/sources.list.d/prebuilt-mpr.list
+}
+
 install_prerequisites() {
   local pkgs="curl file git"
 
@@ -64,7 +69,8 @@ install_prerequisites() {
       fi
       ;;
     "debian")
-      sudo apt update &&
+      setup_debian_prebuilt_mpr &&
+        sudo apt update &&
         sudo apt install -y build-essential &&
         sudo apt install -y $pkgs
       ;;
