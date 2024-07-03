ae9b4ab34945a10bea019fe37a14196b90070d1a
Ryan Perry-Nguyen <ryanperrynguyen@gmail.com>
2024-07-03 19:04:29 -0400

    chore: disable scm thing

--- modules/scm_breeze/.config/shell_post.d/scm_breeze.sh
+++ modules/scm_breeze/.config/shell_post.d/scm_breeze.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+export SCM_BREEZE_DISABLE_ASSETS_MANAGEMENT=true
+
 # load scm_breeze
 SCM_BREEZE_PATH="${XDG_OPT_HOME}/scm_breeze/scm_breeze.sh"
 [ -s "${SCM_BREEZE_PATH}" ] && source "${SCM_BREEZE_PATH}"
