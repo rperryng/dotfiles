b1c5e88d285de672eba16ce14d87045fd537dec4
Ryan Perry-Nguyen <ryanperrynguyen@gmail.com>
2024-07-02 21:47:35 -0400

    chore: add info log for list-fonts cmd

--- modules/fonts/.config/shell.d/fonts.sh
+++ modules/fonts/.config/shell.d/fonts.sh
@@ -6,5 +6,6 @@ list-fonts() {
     return 1
   fi
 
+  echo "Loading fonts using 'system_profiler', this may take a few seconds ..."
   system_profiler -json SPFontsDataType | grep \"family | sort | uniq
 }
