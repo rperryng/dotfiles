e3a60931e5e0e3cc05d4bf04f80cfd2f53949c9d
Ryan Perry-Nguyen <ryanperrynguyen@gmail.com>
2024-07-02 21:09:36 -0400

    chore: add macos keyrepeat speeds

--- modules/macos/install.sh
+++ modules/macos/install.sh
@@ -3,10 +3,20 @@
 
 set -e
 
+set_defaults() {
+  # normal minimum is 15 (225 ms)
+  defaults write -g InitialKeyRepeat -int 10
+
+  # normal minimum is 2 (30 ms)
+  defaults write -g KeyRepeat -int 1
+}
+
 install() {
   if [[ "${DOTFILES_OS}" != 'macos' ]]; then
     return 0;
   fi
+
+  set_defaults
 
   brew install --cask \
     easy-move-plus-resize \
