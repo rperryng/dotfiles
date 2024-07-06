b9d9def8cf6f1b4068d5b6c5316bc1c2494cc0e5
Ryan Perry-Nguyen <ryanperrynguyen@gmail.com>
2024-07-06 14:51:56 -0400

    chore: disable which-key nowait for registers

--- modules/neovim-lua/.config/nvim-lua/lua/plugins/which-key.lua
+++ modules/neovim-lua/.config/nvim-lua/lua/plugins/which-key.lua
@@ -1,12 +1,18 @@
 return {
-  "folke/which-key.nvim",
-  event = "VeryLazy",
-  init = function()
-    -- set in 'options.lua'
-    -- vim.o.timeout = ...
-    -- vim.o.timeoutlen = ...
-  end,
-  opts = {
-    -- use default
-  }
+  'folke/which-key.nvim',
+  event = 'VeryLazy',
+  opts = {},
+  triggers_nowait = {
+    -- marks
+    "`",
+    "'",
+    "g`",
+    "g'",
+    -- spelling
+    "z=",
+
+    -- Remove nowait for registers
+    -- '"',
+    -- "<c-r>",
+  },
 }
