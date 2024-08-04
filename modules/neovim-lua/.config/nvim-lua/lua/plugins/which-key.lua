63ae01d98843a0db924e79c3ae143eef3cde8956
Ryan Perry-Nguyen <ryanperrynguyen@gmail.com>
2024-08-04 03:58:26 -0400

    chore: update plugins

--- modules/neovim-lua/.config/nvim-lua/lua/plugins/which-key.lua
+++ modules/neovim-lua/.config/nvim-lua/lua/plugins/which-key.lua
@@ -5,20 +5,22 @@ if utils.is_wsl() then
 end
 
 return {
-  'folke/which-key.nvim',
-  event = 'VeryLazy',
-  opts = {},
-  triggers_nowait = {
-    -- marks
-    "`",
-    "'",
-    "g`",
-    "g'",
-    -- spelling
-    "z=",
-
-    -- Remove nowait for registers
-    -- '"',
-    -- "<c-r>",
+  {
+    'folke/which-key.nvim',
+    event = 'VeryLazy',
+    opts = {
+      delay = function(_ctx)
+        return 200
+      end,
+    },
+    keys = {
+      {
+        "'",
+        function()
+          require('which-key').show('`')
+        end,
+        desc = 'Marks',
+      },
+    },
   },
 }
