a7ba7d1cd280b6048101b7fc8341cabcfcb5cdd3
Ryan Perry-Nguyen <ryanperrynguyen@gmail.com>
2024-08-04 11:00:21 -0400

    feat: markdown nvim

---
+++ modules/neovim-lua/.config/nvim-lua/lua/plugins/markdown.lua
@@ -0,0 +1,27 @@
+return {
+  {
+    'MeanderingProgrammer/markdown.nvim',
+    -- main = 'render-markdown',
+    config = function()
+      require('render-markdown').setup({})
+
+      vim.api.nvim_create_autocmd('FileType', {
+        pattern = 'markdown',
+        callback = function()
+          vim.api.nvim_buf_set_keymap(
+            0,
+            'n',
+            '<space>mt',
+            ':RenderMarkdown toggle<cr>',
+            { noremap = true, silent = true }
+          )
+        end,
+      })
+    end,
+    name = 'render-markdown', -- Only needed if you have another plugin named markdown.nvim
+    dependencies = {
+      'nvim-treesitter/nvim-treesitter',
+      'nvim-tree/nvim-web-devicons',
+    }, -- if you prefer nvim-web-devicons
+  },
+}
