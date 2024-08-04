ce0fda33144863536db3c71b99df73f8d40933e4
Ryan Perry-Nguyen <ryanperrynguyen@gmail.com>
2024-08-03 22:22:02 -0400

    feat(neovim-lua): safer base config loading

--- modules/neovim-lua/.config/nvim-lua/init.lua
+++ modules/neovim-lua/.config/nvim-lua/init.lua
@@ -6,17 +6,20 @@
 --   * test framework
 --   * debug framework
 
-local function loadbaseconfig()
-  local utils = require('utils')
-  utils.try_require('options')
-  utils.try_require('keymaps')
-  utils.try_require('augroups')
-  utils.try_require('ui')
-end
+-- local utils = require('utils')
+-- utils.try_require('options')
+-- utils.try_require('keymaps')
+-- utils.try_require('augroups')
+-- utils.try_require('ui')
 
-xpcall(loadbaseconfig, function(err)
-  print(debug.traceback(err))
-end)
+local base_config_modules = { 'utils', 'options', 'keymaps', 'augroups', 'ui' }
+for _, module_name in ipairs(base_config_modules) do
+  xpcall(function()
+    require(module_name)
+  end, function(err)
+    print(debug.traceback(err))
+  end)
+end
 
 -- Load plugins separately from other configs, so that errors in my
 -- config files don't stop _everything_ from loading
