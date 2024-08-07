2ca11cb50c9a345661de3278acbd3f6e469d03ce
Ryan Perry-Nguyen <ryanperrynguyen@gmail.com>
2024-08-07 17:02:11 -0400

    feat: switch to gitsigns

--- modules/neovim-lua/.config/nvim-lua/lua/plugins/git.lua
+++ modules/neovim-lua/.config/nvim-lua/lua/plugins/git.lua
@@ -1,74 +1,84 @@
 return {
+  -- {
+  --   'airblade/vim-gitgutter',
+  --   init = function()
+  --     vim.g.gitgutter_map_keys = 0
+  --   end,
+  --   config = function()
+  --     vim.cmd([[
+  --       highlight! link GitGutterAdd GruvboxGreenSign
+  --     ]])
+  --
+  --     -- Hunk Navigation
+  --     vim.keymap.set(
+  --       'n',
+  --       '<space>gqg',
+  --       ':GitGutterQuickFix | copen | cfirst<cr>',
+  --       { desc = 'Load git hunks into quickfix list' }
+  --     )
+  --     vim.keymap.set(
+  --       'n',
+  --       ']g',
+  --       '<Plug>(GitGutterNextHunk)',
+  --       { desc = 'Next git hunk', noremap = false }
+  --     )
+  --     vim.keymap.set(
+  --       'n',
+  --       '[g',
+  --       '<Plug>(GitGutterPrevHunk)',
+  --       { desc = 'Next git hunk', noremap = false }
+  --     )
+  --
+  --     -- Hunk Operations
+  --     vim.keymap.set(
+  --       'n',
+  --       '<space>ghp',
+  --       ':GitGutterPreviewHunk<cr>',
+  --       { desc = 'Preview git diff hunk' }
+  --     )
+  --     vim.keymap.set(
+  --       'n',
+  --       '<space>ghu',
+  --       ':GitGutterUndoHunk<cr>',
+  --       { desc = 'Undo git diff hunk' }
+  --     )
+  --
+  --     -- Text objects
+  --     vim.keymap.set(
+  --       'o',
+  --       'igh',
+  --       '<Plug>(GitGutterTextObjectInnerPending)',
+  --       { desc = 'Git hunk inner' }
+  --     )
+  --     vim.keymap.set(
+  --       'o',
+  --       'agh',
+  --       '<Plug>(GitGutterTextObjectOuterPending)',
+  --       { desc = 'Git hunk inner' }
+  --     )
+  --     vim.keymap.set(
+  --       'x',
+  --       'igh',
+  --       '<Plug>(GitGutterTextObjectInnerVisual)',
+  --       { desc = 'Git hunk inner' }
+  --     )
+  --     vim.keymap.set(
+  --       'x',
+  --       'agh',
+  --       '<Plug>(GitGutterTextObjectOuterVisual)',
+  --       { desc = 'Git hunk inner' }
+  --     )
+  --   end,
+  -- },
+
   {
-    'airblade/vim-gitgutter',
-    init = function()
-      vim.g.gitgutter_map_keys = 0
-    end,
+    'lewis6991/gitsigns.nvim',
     config = function()
-      vim.cmd([[
-        highlight! link GitGutterAdd GruvboxGreenSign
-      ]])
+      local gitsigns = require('gitsigns')
+      gitsigns.setup()
 
-      -- Hunk Navigation
-      vim.keymap.set(
-        'n',
-        '<space>gqg',
-        ':GitGutterQuickFix | copen | cfirst<cr>',
-        { desc = 'Load git hunks into quickfix list' }
-      )
-      vim.keymap.set(
-        'n',
-        ']g',
-        '<Plug>(GitGutterNextHunk)',
-        { desc = 'Next git hunk', noremap = false }
-      )
-      vim.keymap.set(
-        'n',
-        '[g',
-        '<Plug>(GitGutterPrevHunk)',
-        { desc = 'Next git hunk', noremap = false }
-      )
 
-      -- Hunk Operations
-      vim.keymap.set(
-        'n',
-        '<space>ghp',
-        ':GitGutterPreviewHunk<cr>',
-        { desc = 'Preview git diff hunk' }
-      )
-      vim.keymap.set(
-        'n',
-        '<space>ghu',
-        ':GitGutterUndoHunk<cr>',
-        { desc = 'Undo git diff hunk' }
-      )
-
-      -- Text objects
-      vim.keymap.set(
-        'o',
-        'igh',
-        '<Plug>(GitGutterTextObjectInnerPending)',
-        { desc = 'Git hunk inner' }
-      )
-      vim.keymap.set(
-        'o',
-        'agh',
-        '<Plug>(GitGutterTextObjectOuterPending)',
-        { desc = 'Git hunk inner' }
-      )
-      vim.keymap.set(
-        'x',
-        'igh',
-        '<Plug>(GitGutterTextObjectInnerVisual)',
-        { desc = 'Git hunk inner' }
-      )
-      vim.keymap.set(
-        'x',
-        'agh',
-        '<Plug>(GitGutterTextObjectOuterVisual)',
-        { desc = 'Git hunk inner' }
-      )
-    end,
+    end
   },
 
   {
