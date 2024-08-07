8849a482eee72dbe1f736f469f83df71715b52d6
Ryan Perry-Nguyen <ryanperrynguyen@gmail.com>
2024-08-07 19:40:14 -0400

    fix: add missing update

--- modules/wezterm/install.sh
+++ modules/wezterm/install.sh
@@ -14,6 +14,7 @@ install_wezterm() {
     "debian")
       curl -fsSL https://apt.fury.io/wez/gpg.key | sudo gpg --yes --dearmor -o /usr/share/keyrings/wezterm-fury.gpg
       echo 'deb [signed-by=/usr/share/keyrings/wezterm-fury.gpg] https://apt.fury.io/wez/ * *' | sudo tee /etc/apt/sources.list.d/wezterm.list
+      sudo apt update
       sudo apt install wezterm
       ;;
     *)
