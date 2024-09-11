3500330944c292620b867de773951e17b969caa9
Ryan Perry-Nguyen <ryanperrynguyen@gmail.com>
2024-09-10 23:26:20 -0400

    feat: add deno support to formatter plugin

--- modules/neovim-lua/.config/nvim-lua/lua/plugins/formatter.lua
+++ modules/neovim-lua/.config/nvim-lua/lua/plugins/formatter.lua
@@ -19,8 +19,37 @@ return {
             require('formatter.filetypes.ruby').rubocop,
           },
 
+          javascript = {
+            require('formatter.filetypes.javascript').prettier,
+          },
+
           typescript = {
-            require('formatter.filetypes.typescript').prettier,
+            function()
+              local cwd = vim.fn.getcwd()
+              local is_deno = vim.fn.filereadable(cwd .. '/deno.json')
+                or vim.fn.filereadable(cwd .. '/deno.jsonc ')
+
+              if is_deno then
+                return {
+                  exe = 'deno',
+                  args = { 'fmt', '-' },
+                  stdin = true,
+                }
+              else
+                return {
+                  exe = "prettier",
+                  args = {
+                    "--stdin-filepath",
+                    util.escape_path(util.get_current_buffer_file_path()),
+                    "--parser",
+                    'typescript',
+                  },
+                  stdin = true,
+                  try_node_modules = true,
+                }
+              end
+            end,
+            -- require('formatter.filetypes.typescript').prettier,
           },
 
           ['*'] = {
