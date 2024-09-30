d47aca1c9bbe0ce73102e4901dbc03a2f58ddf05
Ryan Perry-Nguyen <ryanperrynguyen@gmail.com>
2024-09-30 18:55:28 -0400

    chore: move core lua

--- modules/neovim-lua/.config/nvim-lua/lua/core.lua
+++ modules/neovim-lua/.config/nvim-lua/lua/core.lua
@@ -3,6 +3,7 @@ for _, module_name in ipairs(base_config_modules) do
   xpcall(function()
     require(module_name)
   end, function(err)
+    print('failed to load module ' .. module_name)
     print(debug.traceback(err))
   end)
 end
