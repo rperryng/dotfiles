8cec7952108d7391826b4d9c6b59814b0c0bc7c1
Ryan Perry-Nguyen <ryanperrynguyen@gmail.com>
2024-10-13 01:49:05 -0400

    chore: move bufremove to mini module

--- modules/neovim-lua/.config/nvim-lua/lua/plugins/mini.lua
+++ modules/neovim-lua/.config/nvim-lua/lua/plugins/mini.lua
@@ -1,5 +1,15 @@
 return {
   {
+    'echasnovski/mini.bufremove',
+    version = '*',
+    event = 'VeryLazy',
+    config = function()
+      require('mini.bufremove').setup()
+
+      vim.keymap.set('n', '<space>bd', '<cmd>lua MiniBufremove.delete(0, true)<cr>', { desc = 'Delete Buffer' })
+    end,
+  },
+  {
     'echasnovski/mini.splitjoin',
     config = function()
       require('mini.splitjoin').setup()
