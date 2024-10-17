6726661db45a0d09c8e646f8f9ce7609d06b1a7b
Ryan Perry-Nguyen <ryanperrynguyen@gmail.com>
2024-10-17 08:57:42 -0400

    feat: projectionist

--- modules/neovim-lua/.config/nvim-lua/lua/plugins/tpope.lua
+++ modules/neovim-lua/.config/nvim-lua/lua/plugins/tpope.lua
@@ -1,4 +1,90 @@
 return {
   { 'tpope/vim-rsi' },
   { 'tpope/vim-eunuch' },
+  {
+    'tpope/vim-projectionist',
+    config = function()
+      vim.keymap.set('n', '<space>a', function()
+        vim.cmd('A')
+      end, { desc = 'Open alternate file'} )
+    end,
+    init = function()
+      vim.g.projectionist_heuristics = {
+        ['Gemfile.lock|*.gemspec'] = {
+          ['*.rb'] = {
+            type = 'source',
+            alternate = 'spec/{}_spec.rb',
+          },
+          ['spec/*_spec.rb'] = {
+            alternate = '{}.rb',
+            type = 'test',
+          },
+        },
+        ['package.json'] = {
+          ['package.json'] = {
+            type = 'package',
+            alternate = { 'yarn.lock', 'package-lock.json' },
+          },
+          ['package-lock.json'] = {
+            alternate = 'package.json',
+          },
+          ['yarn.lock'] = {
+            alternate = 'package.json',
+          },
+          ['src/*.ts'] = {
+            type = 'source',
+            alternate = {
+              'src/{}.test.ts',
+              'src/{}.spec.ts',
+              'test/{}.test.ts',
+              'test/{}.spec.ts',
+            },
+          },
+          ['src/*.test.ts'] = {
+            type = 'test',
+            alternate = 'src/{}.ts',
+          },
+          ['src/*.spec.ts'] = {
+            type = 'test',
+            alternate = 'src/{}.ts',
+          },
+          ['src/*.tsx'] = {
+            type = 'source',
+            alternate = {
+              'src/{}.test.tsx',
+              'src/{}.spec.tsx',
+              'test/{}.test.tsx',
+              'test/{}.spec.tsx',
+            },
+          },
+          ['src/*.test.tsx'] = {
+            type = 'test',
+            alternate = 'src/{}.tsx',
+          },
+          ['src/*.spec.tsx'] = {
+            type = 'test',
+            alternate = 'src/{}.tsx',
+          },
+        },
+        ['pom.xml'] = {
+          ['src/main/java/*.java'] = {
+            type = 'source',
+            alternate = 'src/test/java/{}Test.java',
+          },
+          ['src/test/java/*Test.java'] = {
+            type = 'test',
+            alternate = 'src/main/java/{}.java',
+          },
+          ['src/main/kotlin/*.kt'] = {
+            type = 'source',
+            alternate = 'src/test/kotlin/{}Test.kt',
+          },
+          ['src/test/kotlin/*Test.kt'] = {
+            type = 'test',
+            alternate = 'src/main/kotlin/{}.kt',
+          },
+        },
+      }
+    end,
+  },
 }
