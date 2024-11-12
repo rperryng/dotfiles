9c34fd353c91379321e324fb0ed1ec29fc54eb22
Ryan Perry-Nguyen <ryanperrynguyen@gmail.com>
2024-11-12 18:39:52 -0500

    fix: codify qmk config generation

--- modules/qmk/install.sh
+++ modules/qmk/install.sh
@@ -3,8 +3,9 @@
 set -eo pipefail
 
 QMK_HOME="${QMK_HOME:-${HOME}/code/qmk/qmk_firmware}"
+QMK_USERSPACE_PATH="${HOME}/code/rperryng/qmk_userspace"
 
-clone_qmk() {
+clone_qmk_firmware() {
   if [[ -d "${QMK_HOME}" ]]; then
     return 0
   fi
@@ -12,6 +13,16 @@ clone_qmk() {
   echo "Cloning qmk_firmware repo"
   mkdir -p "${QMK_HOME}"
   git clone git@github.com:qmk/qmk_firmware.git "${QMK_HOME}"
+}
+
+clone_qmk_userspace() {
+  if [[ -d "${QMK_USERSPACE_PATH}" ]]; then
+    return 0
+  fi
+
+  echo "Cloning qmk_userspace repo"
+  mkdir -p "${QMK_USERSPACE_PATH}"
+  git clone git@github.com:rperryng/qmk_userspace.git "${QMK_USERSPACE_PATH}"
 }
 
 install() {
@@ -29,7 +40,16 @@ install() {
   fi
 
   qmk setup --yes
+
+  # Codify the config generation since the file itself cannot resolve env vars
+  # (e.g. setting the overlay dir which is machine-specific), so symlinking the
+  # config file is not viable.
+  qmk config \
+    user.overlay_dir="$(realpath "${QMK_USERSPACE_PATH}")" \
+    user.keyboard='zsa/moonlander' \
+    user.keymap='rperryng'
 }
 
-clone_qmk
+clone_qmk_firmware
+clone_qmk_userspace
 install
