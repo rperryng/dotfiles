7a0e6005e872cb2c0e16e48c3b160244ce9aef2b
Ryan Perry-Nguyen <ryanperrynguyen@gmail.com>
2024-11-18 23:58:38 -0500

    chore: add vrapper config

--- modules/neovim-lua/.config/nvim-lua/lua/local/filetypes.lua
+++ modules/neovim-lua/.config/nvim-lua/lua/local/filetypes.lua
@@ -1,5 +1,17 @@
 vim.filetype.add({
   pattern = {
     ['.*/ghostty/config'] = { 'dosini', { priority = 10 } },
+    ['${DOTFILES_DIR}/modules/git/.config/git/config.*'] = { 'gitconfig', { priority = 10 } },
+  },
+  filename = {
+    ['.vrapper'] = 'vim',
   },
 })
+-- Log('this should be running ...')
+--
+-- vim.filetype.add({
+--   pattern = {
+--     ['$HOME/.config/git/*'] = 'gitconfig',
+--     ['$HOME/.dotfiles/modules/git/.config/git/config*'] = 'gitconfig',
+--   },
+-- })
