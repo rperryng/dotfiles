237bcf43cd56c1d667060f45a56e6b98a1824ace
Ryan Perry-Nguyen <rperrynguyen@wealthsimple.com>
2025-03-21 17:34:59 -0400

    feat: use hyper key to focus window

--- modules/hammerspoon/install.sh
+++ modules/hammerspoon/install.sh
@@ -2,6 +2,26 @@
 
 set -e -o pipefail
 
+SPOONS_INSTALL_DIR="$HOME/.config/hammerspoon/Spoons"
+
+install_spoons() {
+  if [[ -d "$SPOONS_INSTALL_DIR" ]]; then
+    return 0;
+  fi
+
+  mkdir "$SPOONS_INSTALL_DIR"
+
+  echo "Installing SpoonInstall..."
+  curl -L0 \
+    https://github.com/Hammerspoon/Spoons/raw/master/Spoons/SpoonInstall.spoon.zip |
+    tar -zx -C "$SPOONS_INSTALL_DIR"
+}
+
+configire_hammerspoon() {
+  echo "Configuring Hammerspoon..."
+  defaults write org.hammerspoon.Hammerspoon MJConfigFile "$HOME/.config/hammerspoon/init.lua"
+}
+
 install() {
   if [[ "${DOTFILES_OS}" != "macos" ]]; then
     return 0;
@@ -12,6 +32,9 @@ install() {
   fi
 
   brew install --cask hammerspoon
+
+  install_spoons
+  configure_hammerspoon
 }
 
 install
