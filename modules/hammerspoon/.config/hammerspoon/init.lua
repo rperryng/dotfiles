f6a475b9d3cf81a3ed6d793ccec05a84f02446f0
Ryan Perry-Nguyen <ryanperrynguyen@gmail.com>
2025-03-29 11:55:31 -0400

    chore: add hammerspoon globals to luarc

--- modules/hammerspoon/.config/hammerspoon/init.lua
+++ modules/hammerspoon/.config/hammerspoon/init.lua
@@ -10,33 +10,18 @@ local hyper = { 'ctrl', 'alt', 'shift', 'cmd' }
 local meh = { 'ctrl', 'alt', 'shift' }
 
 -- Curried launch by app id
-function launchById(id)
+local function launchById(id)
   return function()
     hs.application.launchOrFocusByBundleID(id)
   end
 end
 
-function openWithFinder(path)
+local function openWithFinder(path)
   return function()
     os.execute('open '..path)
     hs.application.launchOrFocus('Finder')
   end
 end
-
--- Window manager
-spoon.SpoonInstall:andUse('MiroWindowsManager', {
-  -- disable = true,
-  config = {
-    GRID = windowGrid,
-  },
-  -- hotkeys = {
-  --   up = { movekey, 'up' },
-  --   right = { movekey, 'right' },
-  --   down = { movekey, 'down' },
-  --   left = { movekey, 'left' },
-  --   fullscreen = { movekey, 'f' },
-  -- },
-})
 
 -- Reload config
 spoon.SpoonInstall:andUse('ReloadConfiguration', {
@@ -57,8 +42,10 @@ spoon.SpoonInstall:andUse('FadeLogo', {
   start = true
 })
 
+spoon.SpoonInstall:andUse('RecursiveBinder');
+
 -- Application hotkeys
-hs.hotkey.bind(hyper, 'p', launchById('com.1password.1password'))
+hs.hotkey.bind(hyper, '1', launchById('com.1password.1password'))
 hs.hotkey.bind(hyper, 'f', launchById('org.mozilla.firefoxdeveloperedition'))
 hs.hotkey.bind(hyper, 's', launchById('com.spotify.client'))
 hs.hotkey.bind(hyper, 't', launchById('com.mitchellh.ghostty'))
@@ -66,6 +53,8 @@ hs.hotkey.bind(hyper, 'c', launchById('com.tinyspeck.slackmacgap'))
 hs.hotkey.bind(hyper, 'm', launchById('com.tdesktop.Telegram'))
 hs.hotkey.bind(hyper, 'n', launchById('notion.id'))
 hs.hotkey.bind(hyper, 'e', launchById('com.apple.finder'))
-hs.hotkey.bind(hyper, 'g', launchById('app.tuple.app'))
+hs.hotkey.bind(hyper, 'p', launchById('app.tuple.app'))
+hs.hotkey.bind(hyper, 'g', launchById('org.godotengine.godot'))
 hs.hotkey.bind(hyper, 'x', launchById('com.todesktop.230313mzl4w4u92')) -- cursor
+hs.hotkey.bind(hyper, 'v', launchById('com.microsoft.VSCode'))
 hs.hotkey.bind(hyper, 'h', openWithFinder('~/Applications/Chrome Apps.localized/Google Meet.app'))
