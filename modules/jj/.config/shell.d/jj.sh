1b58504289d11a48e5f1b9c032feaf90ca6b26fa
Ryan Perry-Nguyen <ryanperrynguyen@gmail.com>
2025-04-23 17:07:26 -0400

    chore: some stuff idk

--- modules/jj/.config/shell.d/jj.sh
+++ modules/jj/.config/shell.d/jj.sh
@@ -9,7 +9,7 @@ alias nlo="nn log"
 fzf-jj-bookmark-widget() {
   setopt localoptions pipefail no_aliases 2> /dev/null
   local bookmark
-  bookmark=$(jj bookmark list --template '{name()}' | fzf --height 40% --reverse --prompt="Select bookmark: ")
+  bookmark=$(jj bookmark list --template 'name ++ "\n"' | fzf --height 40% --reverse --prompt="Select bookmark: ")
   if [[ -n "$bookmark" ]]; then
     LBUFFER="${LBUFFER}${bookmark} "
   fi
@@ -19,3 +19,43 @@ fzf-jj-bookmark-widget() {
 # Bind the function to ctrl-b
 zle -N fzf-jj-bookmark-widget
 bindkey '^B' fzf-jj-bookmark-widget
+
+# jj moves tracked bookmarks automatically when running fetch.
+# in git, 'fetch' will only update remote references, not update local branch pointers.
+alias npl="jj git fetch"
+alias ngf="jj git fetch"
+alias nlo="jj log -n 6"
+alias nloa="jj log -r '@ | root() | bookmarks()'"
+alias nnew="jj new 'trunk()'"
+
+get_tracking_branch() {
+  local rev="${1:-@}"
+  jj bookmark list --tracked --template "if(tracking_present, name)" --revisions "${rev}" \
+    || die "Failed to get tracking branch"
+}
+
+nlor() {
+  if [[ $# -ne 1 ]]; then
+    echo "Usage: nlor <rev>"
+    return 1
+  fi
+
+  local rev=$1
+  local fork="fork_point(trunk() | $rev)"
+  jj log --revisions "$fork::trunk() | $fork::$rev"
+}
+
+open_pr() {
+  # TODO...
+  revision="@"
+  jj git push --change "${revision}" || die "Failed to push changes"
+
+  local branch
+  branch=$(get_tracking_branch "${revision}") || die "Failed to get branch after push"
+  [ -z "${branch}" ] && die "No branch created after push"
+
+  gh pr create --web "${branch}" \
+    || die "Failed to create PR for branch '${branch}'"
+
+  echo "Successfully created$([ "${draft}" = "true" ] && echo " draft") PR for branch '${branch}'"
+}
