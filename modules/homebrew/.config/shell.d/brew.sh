570db5b5e25631a298b5b1476571c24acf260256
Ryan Perry-Nguyen <ryanperrynguyen@gmail.com>
2025-06-13 16:10:16 -0400

    brew

--- modules/homebrew/.config/shell.d/brew.sh
+++ modules/homebrew/.config/shell.d/brew.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 
+if [[ ${DOTFILES_OS} == "macos" ]]; then
+  PATH="/home/linuxbrew/.linuxbrew/bin:${PATH}"
+  eval "$(/opt/homebrew/bin/brew shellenv)"
+fi
+
 if [[ ${DOTFILES_OS} == "debian" ]]; then
   PATH="/home/linuxbrew/.linuxbrew/bin:${PATH}"
   eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
