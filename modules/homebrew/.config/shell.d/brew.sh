07d61757130e2279e3de69ed8816a2510574326f
Ryan Perry-Nguyen <ryanperrynguyen@gmail.com>
2025-11-18 20:17:25 -0500

    fix: support brew for x86 and arm macos

--- modules/homebrew/.config/shell.d/brew.sh
+++ modules/homebrew/.config/shell.d/brew.sh
@@ -1,8 +1,15 @@
 #!/usr/bin/env bash
 
 if [[ ${DOTFILES_OS} == "macos" ]]; then
-  export PATH="/opt/homebrew/bin:${PATH}"
-  eval "$(/opt/homebrew/bin/brew shellenv)"
+  if [[ $(uname -m) == "x86_64" ]]; then
+    export PATH="/usr/local/bin:${PATH}"
+    eval "$(/usr/local/bin/brew shellenv)"
+  elif [[ $(uname -m) == "arm64" ]]; then
+    export PATH="/opt/homebrew/bin:${PATH}"
+    eval "$(/opt/homebrew/bin/brew shellenv)"
+  else
+    echo "Don't know how to setup brew for macos with architecture '$(uname -m)'" 1>&2
+  fi
 fi
 
 if [[ ${DOTFILES_OS} == "debian" ]]; then
