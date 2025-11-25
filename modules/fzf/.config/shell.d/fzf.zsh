51db17bcb2fc0c855011ef4cf3dbe369ecf8d617
Ryan Perry-Nguyen <ryanperrynguyen@gmail.com>
2025-11-25 10:13:58 -0500

    feat: allow toggle of --no-ignore for fzf ctrl-t

--- modules/fzf/.config/shell.d/fzf.zsh
+++ modules/fzf/.config/shell.d/fzf.zsh
@@ -9,6 +9,7 @@ if [ -x "$(command -v rg)" ]; then
   export FZF_DEFAULT_COMMAND='rg --files --hidden --follow --glob "!.git"'
   export FZF_ALT_C_COMMAND='rg --files --hidden --follow --glob "!.git" | xargs dirname | sort -u'
   export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
+  export FZF_CTRL_T_OPTS="--bind 'ctrl-d:reload(eval \"${FZF_DEFAULT_COMMAND} --no-ignore\"),ctrl-f:reload(eval \"$FZF_DEFAULT_COMMAND\")'"
 fi
 
 prepend_path "${XDG_OPT_HOME}/fzf/bin"
