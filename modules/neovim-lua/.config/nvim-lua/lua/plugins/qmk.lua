067ed969bde8ad3af2e549fdaeae0965591b3f8f
Ryan Perry-Nguyen <ryanperrynguyen@gmail.com>
2026-02-01 18:33:07 -0500

    feat: keyball39 qmk layout

--- modules/neovim-lua/.config/nvim-lua/lua/plugins/qmk.lua
+++ modules/neovim-lua/.config/nvim-lua/lua/plugins/qmk.lua
@@ -60,7 +60,8 @@ return {
         end,
       })
 
-      -- DASBOB
+      -- DASBOBgs
+      --
       vim.api.nvim_create_autocmd('BufEnter', {
         desc = 'Format DASBOB layout',
         group = augroup,
@@ -73,6 +74,33 @@ return {
               'x x x x x _ _ _ x x x x x',
               'x x x x x _ _ _ x x x x x',
               '_ _ _ x x x _ x x x _ _ _',
+            },
+          })
+        end,
+      })
+
+      -- Keyball39
+      --
+      -- Uses LAYOUT_universal (an alias for LAYOUT_no_ball), which is 42 keys:
+      -- 10 per row for the top three rows, then a 12-key thumb row. The thumb
+      -- row has no gap because the inner thumb keys tuck underneath the space
+      -- between the halves, so it spans all 12 columns.
+      --
+      -- Note: on a right-ball build three of those thumb positions (R31/R32/R33)
+      -- do not physically exist -- that is where the trackball sits -- but they
+      -- are still present in the layout macro, so they are counted here.
+      vim.api.nvim_create_autocmd('BufEnter', {
+        desc = 'Format Keyball39 layout',
+        group = augroup,
+        pattern = '*keyball/keyball39/keymaps/rperryng/keymap.c',
+        callback = function()
+          qmk.setup({
+            name = 'LAYOUT_universal',
+            layout = {
+              'x x x x x _ _ _ x x x x x',
+              'x x x x x _ _ _ x x x x x',
+              'x x x x x _ _ _ x x x x x',
+              'x x x x x x _ x x x x x x',
             },
           })
         end,
