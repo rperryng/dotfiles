a525cb134cdbdbc3c93179796254df8fa7b311e9
Ryan Perry-Nguyen <ryanperrynguyen@gmail.com>
2026-09-12 22:37:40 -0400

    chore: remove outdated fzf install script

--- modules/fzf/.config/zsh.d/fzf.zsh
+++ modules/fzf/.config/zsh.d/fzf.zsh
@@ -1,7 +1,6 @@
 #!/usr/bin/env zsh
 
-source "${XDG_OPT_HOME}/fzf/shell/completion.zsh"
-source "${XDG_OPT_HOME}/fzf/shell/key-bindings.zsh"
+source <(fzf --zsh)
 
 # CTRL-Y - cd into the selected directory
 bindkey -M emacs '^Y' fzf-cd-widget
